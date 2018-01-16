a7473f50cc05aacfefb58bb2e34323bf4bb5e68e
Oncioiu Dan <oncioiu_dan_ionut@yahoo.com>
2018-01-16 14:13:34 +0200

    add file ion.java

---
+++ ion.java
@@ -0,0 +1,17 @@
+ Volume in drive C is System
+ Volume Serial Number is 08E5-9B69
+
+ Directory of C:\batfile
+
+01/16/2018  02:12 PM    <DIR>          .
+01/16/2018  02:12 PM    <DIR>          ..
+01/16/2018  02:08 PM               415 abc.class
+08/25/2017  02:09 PM               103 abc.java
+12/14/2017  05:07 PM    <DIR>          dev
+01/16/2018  02:12 PM                 0 ion.java
+12/14/2017  05:04 PM                39 promote.bat
+01/16/2018  02:05 PM               621 readme.txt
+01/16/2018  01:51 PM    <DIR>          test
+08/25/2017  02:52 PM                88 test.bat
+               6 File(s)          1,266 bytes
+               4 Dir(s)  375,202,271,232 bytes free
